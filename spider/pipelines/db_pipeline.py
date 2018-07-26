ed8784dd30eac98efa057b656ae6370670514779
Lei Wu <leiwu@Leis-MBP.fios-router.home>
2018-07-25 21:50:36 -0400

    add psycopg support

--- spider/pipelines/db_pipeline.py
+++ spider/pipelines/db_pipeline.py
@@ -1,8 +1,25 @@
+import psycopg2, configparser
+
 from spider.item.address import Address
 
 
 class DB_Pipeline(object):
+    conn = None
+    cursor = None
+
+    def open_spider(self, spider):
+        config = configparser.ConfigParser()
+        config.read('/etc/opt/spider/config.ini')
+        connect_str = config['database']['ConnectStr']
+        self.conn = psycopg2.connect(connect_str)
+        self.cursor = self.conn.cursor()
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.conn.close()
+
     def process(self, item, spider):
         if isinstance(item, Address):
-            print(item)
+            self.cursor.execute('insert into address (address, town, state, zipcode) values (%s, %s, %s, %s)',
+                                (item.address, item.town, item.state, item.zipcode))
         return item
